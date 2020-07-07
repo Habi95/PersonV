5a310f3167d804b8857baace509093bbf2770b98
Habi95 <Habi95@users.noreply.github.com>
2020-07-07 09:45:11 +0200

    added material neede classes

---
+++ PersonenVerwaltung/PersonData/model/interfaces/IMaterial.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonData.model.interfaces
+{
+    interface IMaterial
+    {
+    }
+}
---
+++ PersonenVerwaltung/PersonData/model/material/equipment.cs
@@ -0,0 +1,20 @@
+﻿using PersonData.model.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonData.model.material
+{
+    public class equipment : IMaterial
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public string make { get; set; }
+        public string model { get; set; }
+        public int? location_id { get; set; }
+        public int? person_id { get; set; }
+        public int quantity { get; set; }
+        //public classroom classroom { get; set; }
+        public Person person { get; set; }
+    }
+}
